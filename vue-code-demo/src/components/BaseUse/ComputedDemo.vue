e163d20c3f8e77bbea81a161d783618577bae4a2
lancer <43966458+lancertea@users.noreply.github.com>
2020-04-20 16:51:01 +0800

    Update ComputedDemo.vue

--- vue-code-demo/src/components/BaseUse/ComputedDemo.vue
+++ vue-code-demo/src/components/BaseUse/ComputedDemo.vue
@@ -16,9 +16,11 @@ export default {
         }
     },
     computed: {
+    //提供的函数用作该属性的getter函数(默认)
         double1() {
             return this.num * 2
         },
+    //计算属性默认只有 getter，不过在需要时你也可以提供一个 setter。
         double2: {
             get() {
                 return this.num * 2
