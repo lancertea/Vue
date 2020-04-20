a2f61c0fc6637524252b88982bc967cce4d8c938
lancer <43966458+lancertea@users.noreply.github.com>
2020-04-20 17:18:56 +0800

    Update ClassDemo.vue

--- vue-code-demo/src/components/BaseUse/ClassDemo.vue
+++ vue-code-demo/src/components/BaseUse/ClassDemo.vue
@@ -1,36 +1,40 @@
 <template>
-    <div>
-        <p :class="{ black: isBlack, yellow: isYellow }">使用 class</p>
-        <p :class="[black, yellow]">使用 class （数组）</p>
-        <p :style="styleData">使用 style</p>
-    </div>
+  <div>
+    <p :class="{ black: isBlack, yellow: isYellow }">使用 class</p>
+    <p :class="[black, yellow]">使用 class （数组）</p>
+    <!-- 绑定内联样式 v-bind:style
+    语法十分直观——看着非常像 CSS，但其实是一个 JavaScript 对象
+    下面这个等同于：
+    <p v-bind:style="{fontSize:40px,color:red,backgroundColor:#ccc}">使用 style</p> -->
+    <p :style="styleData">使用 style</p>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            isBlack: true,
-            isYellow: true,
+  data() {
+    return {
+      isBlack: true,
+      isYellow: true,
 
-            black: 'black',
-            yellow: 'yellow',
+      black: "black",
+      yellow: "yellow",
 
-            styleData: {
-                fontSize: '40px', // 转换为驼峰式
-                color: 'red',
-                backgroundColor: '#ccc' // 转换为驼峰式
-            }
-        }
-    }
-}
+      styleData: {
+        fontSize: "40px", // 转换为驼峰式
+        color: "red",
+        backgroundColor: "#ccc" // 转换为驼峰式
+      }
+    };
+  }
+};
 </script>
 
 <style scoped>
-    .black {
-        background-color: #999;
-    }
-    .yellow {
-        color: yellow;
-    }
+.black {
+  background-color: #999;
+}
+.yellow {
+  color: yellow;
+}
 </style>
