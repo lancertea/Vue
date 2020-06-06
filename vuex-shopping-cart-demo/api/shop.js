8c9cbda473753b1964fb36589cd81b5c35a511a6
lancer <43966458+lancertea@users.noreply.github.com>
2020-06-06 16:07:18 +0800

    update

--- vuex-shopping-cart-demo/api/shop.js
+++ vuex-shopping-cart-demo/api/shop.js
@@ -25,37 +25,3 @@ export default {
   }
 }
 
-/*
-数据结构设计：
-{productionList:[
-  {
-    id:1,
-    title:"商品1",
-    price:10
-  },
-  ...
-],
-cartList:[
-  {
-  id:1,
-  quantity:1
-  },
-  ...
-]
-}
-
-
-组件设计
-<App>管理所有数据 
-  <ProductionList>   商品列表
-      <ProductionListItem/>
-      <ProductionListItem/>
-      <ProductionListItem/>
-  </ProductionList> 
-  <CartList>         购物车列表和总价
-     <CartItem/>
-     <CartItem/>
-  </CartList>
-</App>
-     
-*/
